19acd4070b46f0bc213dbbe37cc3667e3c1d651f
rodov <rodov.michael@gmail.com>
2017-02-01 00:09:20 +0200

    - removed xhttp and replaced it with native es6 fetch - fixed icons on game details - updated excluded players

--- src/js/main.js
+++ src/js/main.js
@@ -8,44 +8,63 @@ import ContentPage from './ContentPage.jsx';
 import {createStore} from 'redux';
 import {urtApp} from './ReduxReducers';
 import {initState} from './ReduxActions';
-import {xhttp} from 'xhttp';
 import AOS from 'aos';
 import * as Constants from './constants';
 
 /*Allow access only for Chrome/Opera/FireFox*/
 if (Browser.name.search(Constants.NOT_SUPPORTED_BROWSERS) > -1) {
-    console.info("browser: "+Browser.name);
+    console.info("browser: " + Browser.name);
     location.href = "../../not-supported.html?browser=" + ((Browser.name == 'ie') ? "InternetExplorer" : Browser.name);
 }
 
 /*Animate On Scroll library init*/
 AOS.init();
 
-xhttp({
-    url: "DATA/games.json",
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    },
-    method: 'GET'},
-    (data, xhr) => {
-        console.log("Data loaded. Loaded " + Object.keys(data.games).length + " games.");
+//fetch the games data
+let headers = new Headers();
+headers.append('Content-Type', 'application/json');
+headers.append('Accept', 'application/json');
+
+let init = {
+    method: 'GET',
+    headers: headers,
+    mode: 'cors',
+    cache: 'default'
+};
+
+let requestGames = new Request("DATA/games.json", init);
+
+fetch(requestGames)
+    .then((response) => {
+        if (response.status != 200) {
+            console.error("Response code expected is 200, but received was " + response.status);
+            return;
+        }
+
+        response.json().then(data => {
+                console.log("Data loaded. Loaded " + Object.keys(data.games).length + " games.");
 
 
-        let reduxStore = createStore(urtApp);
-        reduxStore.dispatch(initState(data));
+                let reduxStore = createStore(urtApp);
+                reduxStore.dispatch(initState(data));
 
-        ReactDOM.render(
-            <ContentPage store={reduxStore}/>,
-            document.getElementById('react-container')
+                ReactDOM.render(
+                    <ContentPage store={reduxStore}/>,
+                    document.getElementById('react-container')
+                );
+
+                //slowly fadein the actual body of the screen
+                document.getElementById("main").className += ' fadein';
+                document.getElementById("loader").className += ' fadeout';
+
+            }
         );
 
-        //slowly fadein the actual body of the screen
-        document.getElementById("main").className += ' fadein';
-        document.getElementById("loader").className += ' fadeout';
-    },
-    (err, xhr) => {
-        console.error(xhr.responseURL, xhr.status, xhr.statusText);
+
+    })
+    .catch(err => {
+        console.error("Failed to fetch games.json. " + err.status + ":" + err.statusText);
     });
+
 
 
